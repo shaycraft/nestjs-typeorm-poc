77246dbebabddce2c19aa8cb4fae08c78f2d0d3a
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-12-24 02:30:09 -0700

    feat: configures postgres typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
+// NestJs
 import { Module } from '@nestjs/common';
+
+// TypeORM
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// controllers
 import { AppController } from './app.controller';
+
+// services
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'changeme',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
